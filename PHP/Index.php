401f87837cc4b377a58bd3efb04e2e8c260b3d23
Alejandro <alemartin99.r@gmail.com>
2024-03-18 10:23:42 +0100

    Header

--- PHP/Index.php
+++ PHP/Index.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
-require_once("../HTML/Index.html");
+include("../PHP/Header.php");
 
 // Limpia las variables de sesión después de guardarlas en variables locales
 unset($_SESSION['successMensaje'], $_SESSION['errorMensaje']);
+
+
+
+
 ?>
