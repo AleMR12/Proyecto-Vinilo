f6dbfe82b3f560910e99a201fc58d3aa9f4f60ec
Alejandro <alemartin99.r@gmail.com>
2024-03-18 09:29:33 +0100

    .

--- PHP/Index.php
+++ PHP/Index.php
@@ -2,7 +2,6 @@
 session_start();
 require_once("../HTML/Index.html");
 
-
 // Limpia las variables de sesión después de guardarlas en variables locales
 unset($_SESSION['successMensaje'], $_SESSION['errorMensaje']);
 ?>
